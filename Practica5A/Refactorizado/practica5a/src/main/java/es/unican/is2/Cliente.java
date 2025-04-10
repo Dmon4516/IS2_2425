0194173dfa1fbc9be2635bd4b5d66540143b47ab
David Montenegro <68624139+Dmon4516@users.noreply.github.com>
2025-04-10 12:23:28 +0200

    P5A Copiar proyecto para refactorizar

---
+++ Practica5A/Refactorizado/practica5a/src/main/java/es/unican/is2/Cliente.java
@@ -0,0 +1,94 @@
+package es.unican.is2;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Cliente { // CCog = 9, CCogn = 9 / 11 = 0,81, WMC = 15, WMCn = 15 / 11 = 1,36
+	
+	public String nombre;
+	public String calle;
+	public String zip;
+	public String localidad;
+	public String telefono;
+	public String dni;
+	
+    private List<Cuenta> Cuentas = new LinkedList<Cuenta>();
+    
+    private List<Tarjeta> tarjetas = new LinkedList<Tarjeta>(); // Estandarizador nombre
+
+ 	public Cliente(String titular, String calle, String zip, String localidad, 
+ 			String telefono, String dni) {  // CCog = 0, WMC = 1
+		this.nombre = titular;
+		this.calle = calle;
+		this.zip = zip;
+		this.localidad = localidad;
+		this.telefono = telefono;
+		this.dni = dni;
+	}
+	
+	public void cambiaDireccion(String calle, String zip, String localidad) { // CCog = 0, WMC = 1
+		this.calle = calle;
+		this.zip = zip;
+		this.localidad = localidad;
+	}
+	
+	public void anhadeCuenta(Cuenta c) { // CCog = 0, WMC = 1
+		Cuentas.add(c); // Cambiar variables
+	}
+	
+	public void anhadeTarjeta(Tarjeta t) { // CCog = 2, WMC = 3
+		tarjetas.add(t); // Cambiar variables
+
+		// Cambiar dentro de la clases abtractas de la tarjeta
+		if (t instanceof Debito) { // CCog + 1, WMC + 1
+			Debito td = (Debito)t;
+			td.getCuentaAsociada().setCaducidadDebito(td.getCaducidadDebito());
+		} else { // CCog + 1, WMC + 1
+			Credito tc = (Credito) t;
+			tc.getCuentaAsociada().setCaducidadCredito(tc.getCaducidadCredito());
+		}
+	}
+	
+	// Revisar
+	public double getSaldoTotal() { // CCog = 7, WMC = 3
+		double total = 0.0;
+		for (Cuenta c: Cuentas) {  // CCog + 1
+			if (c instanceof CuentaAhorro) { // CCog + 2 (nesting=1), WMC + 1
+				total += ((CuentaAhorro) c).getSaldo();
+			} else if (c instanceof CuentaValores)  { // CCog + 1, WMC + 1
+				for (Valor v: ((CuentaValores) c).getValores()) { // CCog + 3 (nesting=2)
+					// Agregar metodo en clase Valor
+					total += v.getCotizacion()*v.getNumValores(); 
+				}
+			}
+		}
+		return total;
+	}
+	
+	public String getNombre() { // CCog = 0, WMC = 1
+		return nombre;
+	}
+
+	public String getCalle() { // CCog = 0, WMC = 1
+		return calle;
+	}
+
+	public String getZip() { // CCog = 0, WMC = 1
+		return zip;
+	}
+
+	public String getLocalidad() { // CCog = 0, WMC = 1
+		return localidad;
+	}
+
+	public String getTelefono() { // CCog = 0, WMC = 1
+		return telefono;
+	}
+
+	public String getDni() { // CCog = 0, WMC = 1
+		return dni;
+	}
+	
+	
+	
+}
---
+++ Practica5A/Refactorizado/practica5a/src/main/java/es/unican/is2/Debito.java
@@ -0,0 +1,47 @@
+package es.unican.is2;
+
+import java.time.LocalDate;
+
+public class Debito extends Tarjeta { // CCog = 2, CCogn = 2 / 6 = 0,3, WMC = 8, WMCn = 8 / 6 = 1,33
+	
+	private double saldoDiarioDisponible;
+
+	public Debito(String numero, String titular, String cvc, CuentaAhorro cuentaAsociada) { // CCog = 0, WMC = 1
+		super(numero, titular, cvc, cuentaAsociada);
+		saldoDiarioDisponible = cuentaAsociada.getLimiteDebito(); 
+	}
+
+	@Override
+	public void retirar(double x) throws saldoInsuficienteException, datoErroneoException { // CCog = 1, WMC = 2
+		if (saldoDiarioDisponible<x) { // CCog + 1, WMC + 1
+			throw new saldoInsuficienteException("Saldo insuficiente");
+		}
+		this.cuentaAsociada.retirar("Retirada en cajero", x);
+		saldoDiarioDisponible-=x;
+	}
+	
+	@Override
+	public void pagoEnEstablecimiento(String datos, double x) throws saldoInsuficienteException, datoErroneoException { // CCog = 1, WMC = 2
+		if (saldoDiarioDisponible<x) {  // CCog + 1, WMC + 1
+			throw new saldoInsuficienteException("Saldo insuficiente");
+		}
+		this.cuentaAsociada.retirar("Compra en : " + datos, x);
+		saldoDiarioDisponible-=x;
+	}
+
+	// Mover al padre
+	public LocalDate getCaducidadDebito() { // CCog = 0, WMC = 1
+		return this.cuentaAsociada.getCaducidadDebito();
+	}
+
+	// Revisar padre y revisar cuenta asociada y cuenta
+	public void restableceSaldo() { // CCog = 0, WMC = 1
+		saldoDiarioDisponible = cuentaAsociada.getLimiteDebito();
+	}
+	
+	// Mover al padre
+	public CuentaAhorro getCuentaAsociada() { // CCog = 0, WMC = 1
+		return cuentaAsociada;
+	}
+
+}
